53ab2a15f4835045759d4d705085293fd4aef4ac
Bamzzzz <muyiwaayobami@yahoo.com>
2024-01-15 07:24:32 -0800

    hmmm

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so "https://raw.githubusercontent.com/Bamzzzz/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so"
-export LD_PRELOAD="$PWD/../libtest.so"
+wget -P ..https://raw.githubusercontent.com/Bamzzzz/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
+export LD_PRELOAD="$PWD/../gigalib.so"
