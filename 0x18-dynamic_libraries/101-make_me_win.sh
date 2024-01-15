dfd989f0ad1a84d42c85cdd8fab3ab8610596e89
Bamzzzz <muyiwaayobami@yahoo.com>
2024-01-15 07:49:07 -0800

    wow

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Bamzzzz/alx-low_level_programming/
-master/0x18-dynamic_libraries/gigalib.so
+wget -P .. https://raw.githubusercontent.com/Bamzzzz/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so
 export LD_PRELOAD="$PWD/../gigalib.so"
