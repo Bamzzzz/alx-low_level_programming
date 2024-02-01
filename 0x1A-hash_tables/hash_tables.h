358bd311026c2fa2c5a72a3ee08cb48d8b5f870f
Bamzzzz <muyiwaayobami@yahoo.com>
2024-02-01 04:38:25 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -14,9 +14,9 @@
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -28,8 +28,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -51,17 +51,17 @@ void hash_table_delete(hash_table_t *ht);
  */
 typedef struct shash_node_s
 {
-	char *key;
-	char *value;
-	struct shash_node_s *next;
-	struct shash_node_s *sprev;
-	struct shash_node_s *snext;
+char *key;
+char *value;
+struct shash_node_s *next;
+struct shash_node_s *sprev;
+struct shash_node_s *snext;
 } shash_node_t;
 
 /**
  * struct shash_table_s - Sorted hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
+ * @size: The size
+ * @array: An array
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  * @shead: A pointer to the first element of the sorted linked list
@@ -69,17 +69,17 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-	unsigned long int size;
-	shash_node_t **array;
-	shash_node_t *shead;
-	shash_node_t *stail;
+unsigned long int size;
+shash_node_t **array;
+shash_node_t *shead;
+shash_node_t *stail;
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+char *shash_table_get(const shash_table_t *ht, const char *key);
 
 #endif /* HASH_TABLES_H */
